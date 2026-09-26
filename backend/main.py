ee08a8752009db0af1d64bcf175fd6f3567180fd
vitthalkharole <vitthalkharole0@gmail.com>
2026-09-26 17:39:16 +0530

    first pull

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from pydantic import BaseModel
 from ai_agent import graph,SYSTEM_PROMPT,parse_response
@@ -19,7 +18,6 @@ async def ask(query: Query):
     inputs = {"messages": [("system", SYSTEM_PROMPT), ("user", query.message)]}
     stream = graph.stream(inputs, stream_mode="updates")
     tool_called_name, final_response = parse_response(stream)
-    print(tool_called_name)
     return {"response":final_response,
             "tool_called":tool_called_name}
 
@@ -30,36 +28,3 @@ async def ask(query: Query):
     
 
    
-=======
-from fastapi import FastAPI
-from pydantic import BaseModel
-from ai_agent import graph,SYSTEM_PROMPT,parse_response
-
-app = FastAPI()
-
-#validation
-class Query(BaseModel):
-    message:str
-
-
-
-@app.post("/ask")
-async def ask(query: Query):
-
-    # AI agent
-    # response = ai_agent(query)
-    inputs = {"messages": [("system", SYSTEM_PROMPT), ("user", query.message)]}
-    stream = graph.stream(inputs, stream_mode="updates")
-    tool_called_name, final_response = parse_response(stream)
-    print(tool_called_name)
-    return {"response":final_response,
-            "tool_called":tool_called_name}
-
-
-            
-            
-
-    
-
-   
->>>>>>> 4e9915bfae2b9f3ca08054dd55fe9501f91e5ff3
